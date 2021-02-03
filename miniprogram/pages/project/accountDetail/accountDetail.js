7ec3e1e1827b0ccd550883c1db135ee5a74ca4d0
T-Oner <1@fxcdev.com>
2021-02-03 22:16:17 +0800

    [config] add wxcharts

--- miniprogram/pages/project/accountDetail/accountDetail.js
+++ miniprogram/pages/project/accountDetail/accountDetail.js
@@ -1,67 +1,67 @@
 // miniprogram/pages/project/accountDetail/accountDetail.js
 
-const Charts = require('../../../dist/wxcharts.js')
+const Charts = require('../../../wxcharts/wxcharts.js')
 
 Page({
 
   data: {
-    title : "吖吖资产",
-    top_menu : "菜单",
-    sum_hidden : false,
-    tran_hidden : false,
-    bottom : getApp().globalData.bottom,
-    list : [
+    title: "吖吖资产",
+    top_menu: "菜单",
+    sum_hidden: false,
+    tran_hidden: false,
+    bottom: getApp().globalData.bottom,
+    list: [
       {
-        title_one : "六月",
-        title_two : "Jun",
-        array :[
-          { 
-            title : "转账",
-            img : '',
-            money : 1000, 
-            bank : "招行",
-            type : "基金",
-            name : "老婆",
-            month : "06",
-            day : "23",
+        title_one: "六月",
+        title_two: "Jun",
+        array: [
+          {
+            title: "转账",
+            img: '',
+            money: 1000,
+            bank: "招行",
+            type: "基金",
+            name: "老婆",
+            month: "06",
+            day: "23",
           },
-          { 
-            title : "调整金额",
-            img : '',
-            money : 1000, 
-            bank : "招行",
-            type : "基金",
-            name : "老婆",
-            month : "06",
-            day : "23",
+          {
+            title: "调整金额",
+            img: '',
+            money: 1000,
+            bank: "招行",
+            type: "基金",
+            name: "老婆",
+            month: "06",
+            day: "23",
           },
 
         ],
-         
+
       },
       {
-        title_one : "七月",
-        title_two : "JuL",
-        array :[
-          { 
-            title : "转账",
-            img : '',
-            money : 1000, 
-            bank : "招行",
-            type : "基金",
-            name : "老婆",
-            month : "06",
-            day : "23",
+        title_one: "七月",
+        title_two: "JuL",
+        array: [
+          {
+            title: "转账",
+            img: '',
+            money: 1000,
+            bank: "招行",
+            type: "基金",
+            name: "老婆",
+            month: "06",
+            day: "23",
           },
-          { 
-            title : "调整金额",
-            img : '',
-            money : 1000, 
-            bank : "招行",
-            type : "基金",
-            name : "老婆",
-            month : "06",
-            day : "23",
+          {
+            title: "调整金额",
+            img: '',
+            money: 1000,
+            bank: "招行",
+            type: "基金",
+            name: "老婆",
+            month: "06",
+            day: "23",
           },
 
         ],
@@ -76,27 +76,27 @@ Page({
     new Charts({
       canvasId: 'Statistical1',
       type: 'column',
-      categories: ['W17', 'W18', 'W19', 'W20', 'W21', 'W22','W23','W24','W25','W26'],
+      categories: ['W17', 'W18', 'W19', 'W20', 'W21', 'W22', 'W23', 'W24', 'W25', 'W26'],
       series: [
         {
-        name :" ",
-        color: '#BDE1F3', 
-        background: '#1F9CEC',
-        data: [5,22,12,2,37,3,15,21,44,8],
-        format: function (val) {
-          return val.toFixed(2);
+          name: " ",
+          color: '#BDE1F3',
+          background: '#1F9CEC',
+          data: [5, 22, 12, 2, 37, 3, 15, 21, 44, 8],
+          format: function (val) {
+            return val.toFixed(2);
+          },
         },
-      },
-    ],
-    yAxis:
-      { 
-        splitNumber : 10,
+      ],
+      yAxis:
+      {
+        splitNumber: 10,
         disableGrid: true,
         format: function (val) {
           return val + 'K';
         },
 
-      },  
+      },
       dataPointShape: true,
       width: 380,
       height: 240
@@ -107,44 +107,44 @@ Page({
     new Charts({
       canvasId: 'Statistical2',
       type: 'line',
-      categories: ['W17', 'W18', 'W19', 'W20', 'W21', 'W22','W23','W24','W25','W26'],
+      categories: ['W17', 'W18', 'W19', 'W20', 'W21', 'W22', 'W23', 'W24', 'W25', 'W26'],
       series: [
         {
-        name :" ",
-        color: '#BDE1F3', 
-        background: '#1F9CEC',
-        data: [5,22,12,2,37,3,15,21,44,8],
-        format: function (val) {
-          return val.toFixed(2);
+          name: " ",
+          color: '#BDE1F3',
+          background: '#1F9CEC',
+          data: [5, 22, 12, 2, 37, 3, 15, 21, 44, 8],
+          format: function (val) {
+            return val.toFixed(2);
+          },
         },
-      },
-    ],
-    yAxis:
-      { 
-        position:'right',
-        splitNumber : 10,
+      ],
+      yAxis:
+      {
+        position: 'right',
+        splitNumber: 10,
         disableGrid: true,
         format: function (val) {
           return val + 'K';
         },
 
-      },  
+      },
       dataPointShape: true,
       width: 380,
       height: 240
     });
   },
-  showDialog : function(e) {
+  showDialog: function (e) {
     var index = e.currentTarget.dataset.id;
     var array = this.data.list[index].array;
     var title = array[index].title
     if (title == "转账") {
       this.setData({
-        tran_hidden : true
+        tran_hidden: true
       })
-    }else{
+    } else {
       this.setData({
-        sum_hidden : true
+        sum_hidden: true
       })
     }
   },
