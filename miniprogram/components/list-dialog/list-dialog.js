4c93ba147485b350c5c66a44f7188dd0f136291b
T-Oner <1@fxcdev.com>
2021-02-18 18:01:25 +0800

    feature: add time picker dialog

--- miniprogram/components/list-dialog/list-dialog.js
+++ miniprogram/components/list-dialog/list-dialog.js
@@ -1,23 +1,38 @@
 // components/list-dialog/list-dialog.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        isHidden: {
+            type: Boolean,
+            value: true
+        }
+    },
 
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {},
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        dismiss: function() {
+            console.log("dismiss")
+            this.setData({
+                'isHidden': true
+            })
+        },
+        doNothing: function() {
+            console.log('doNothing');
+        },
+        _propertyChange: function(newVal, oldVal) {
+            console.log('_propertyChange');
+        }
+    },
+    attached: function() {
+        console.log("attached " + this.data.data);
+    }
 })
