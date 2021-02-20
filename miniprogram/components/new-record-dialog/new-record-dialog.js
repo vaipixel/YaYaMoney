3f8681f655ca34cd13643303a93019783ce517e3
T-Oner <1@fxcdev.com>
2021-02-20 10:47:57 +0800

    feature: reformat form

--- miniprogram/components/new-record-dialog/new-record-dialog.js
+++ miniprogram/components/new-record-dialog/new-record-dialog.js
@@ -1,23 +1,29 @@
 // components/new-record-dialog/new-record-dialog.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    options: {
+        virtualHost: true
+    },
+    /**
+     * 组件的属性列表
+     */
+    properties: {
 
-  },
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        filterData: [
+            '调整余额',
+            '转账'
+        ]
+    },
 
-  },
+    /**
+     * 组件的方法列表
+     */
+    methods: {
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+    }
 })
